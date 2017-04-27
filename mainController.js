a761f26c9cfc172be88bffb21337373a423b99e2
James Green <jamesg@ensemble.com>
2017-04-27 10:46:38 +0100

    Change to promises so that the dom is updated when all the results are in

--- mainController.js
+++ mainController.js
@@ -1,60 +1,51 @@
-app.controller('mainController', function ($scope) {
+app.controller('mainController', function ($scope, $q) {
     var dbx = null;
+    $scope.files = [];
 
-    $scope.listFiles = function () {
+    $scope.showFileList = function () {
+        listFiles().then(files => {
+            $scope.files.length = 0;
+            // apply once we have all the files and urls
+            $scope.$apply(function () {
+                files.forEach(e => $scope.files.push(e));
+            });
+        });
+    }
+
+    listFiles = function () {
         var ACCESS_TOKEN = '5TvwQ0C2DlAAAAAAAAAADFU4naUdxFM-NjDCoJWAxLnCH8IK1VR1W37LhBd2l2mp';
         dbx = new Dropbox({
             accessToken: ACCESS_TOKEN
         });
-        dbx.filesListFolder({
+
+        return dbx.filesListFolder({
                 path: ''
             })
             .then(function (response) {
-                displayFiles(response.entries);
-                console.log(response);
-
+                // return the list of files
+                return response.entries
+            })
+            .then(filelist => {
+                // return once all of the urls for each file are in
+                return $q.all(
+                    filelist.map(file => dbx.sharingCreateSharedLink({
+                        path: file.path_lower
+                    }))
+                );
+            })
+            .then(response => {
+                return response.map(r => {
+                    // return object containing the filename and url
+                    return {
+                        'filename': r.path.replace('/', ''),
+                        'url': r.url
+                    }
+                });
             })
             .catch(function (error) {
                 console.error(error);
             });
-
-
-        return false;
     }
-
-    function displayFiles(files) {
-        var filesList = document.getElementById('files');
-        var li;
-        var link;
-        for (var i = 0; i < files.length; i++) {
-            console.log('file = ', files[i]);
-            link = document.createElement('a');
-            link.setAttribute('id', files[i].path_lower)
-
-            li = document.createElement('li');
-            li.appendChild(document.createTextNode(files[i].name));
-
-            link.appendChild(li);
-            filesList.appendChild(link);
-
-            dbx.sharingCreateSharedLink({
-                    path: files[i].path_lower
-                })
-                .then(function (response) {
-                    ahref = document.getElementById(response.path);
-                    ahref.setAttribute('href', response.url);
-
-                    console.log('get share ', response.path);
-                    console.log('get share2 ', response);
-                })
-                .catch(function (error) {
-                    console.error(error);
-                });
-        }
-
-
-
-    };
 
 
 });
