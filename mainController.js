8ae5bbdc2ad3dc64a8241c5db0150a317ac831c3
James Green <jamesg@ensemble.com>
2017-04-28 11:11:35 +0100

    Email links

--- mainController.js
+++ mainController.js
@@ -3,7 +3,7 @@ app.controller('mainController', function ($scope, $q) {
     $scope.files = [];
     $scope.selection = {};
     var filesToShare = [];
-    var filesToShare2 = [];
+
     $scope.showFileList = function () {
         listFiles().then(files => {
             $scope.files.length = 0;
@@ -57,102 +57,53 @@ app.controller('mainController', function ($scope, $q) {
                     'filename': file.filename,
                     'url': file.url
                 });
-                filesToShare2.push(file.url);
-                $scope.test = filesToShare2;
             }
         });
         console.log('share = ', filesToShare);
         if (filesToShare.length != 0) {
-            share(filesToShare);
+            shareViaEmail(filesToShare);
         }
 
     };
 
-    var share = function (filesToShare) {
-        console.log('sharing ', filesToShare);
-        window.plugins.socialsharing.shareWithOptions(options1, onSuccess, onError);
-    };
-    var share = function (filesToShare) {
-        window.plugins.socialsharing.shareWithOptions(options2, onSuccess, onError);
-    };
 
-    // this is the complete list of currently supported params you can pass to the plugin (all optional)
-    var options1 = {
-        message: 'The folowing files have been shared with you', // not supported on some apps (Facebook, Instagram)
-        subject: 'the subject', // fi. for email
-        files: filesToShare, // an array of filenames either locally or remotely
-        url: 'http://google.com',
-        chooserTitle: 'Select an app to share with' // Android only, you can override the default share sheet title
-    }
-    // this is the complete list of currently supported params you can pass to the plugin (all optional)
-    var options2 = {
-        message: 'The array is full of links this time', // not supported on some apps (Facebook, Instagram)
-        subject: 'the subject', // fi. for email
-        files: filesToShare2, // an array of filenames either locally or remotely
-        url: 'http://google.com',
-        chooserTitle: 'Select an app to share with' // Android only, you can override the default share sheet title
-    }
+    var shareViaEmail = function (filesToShare) {
+
+        var html = createHTMLlinks(filesToShare);
+        // check for a configured email client
+        window.plugins.socialsharing.canShareViaEmail( // share something
+            window.plugins.socialsharing.shareViaEmail(
+                html,
+                'Share PDFs via email',
+                null, // TO: must be null or an array
+                null, // CC: must be null or an array
+                null, // BCC: must be null or an array
+                null, // FILES: null, a string, or an array
+                onSuccess, // called when email was sent or canceled, no way to differentiate
+                onError // called when something unexpected happened
+            ));
+    };
     var onSuccess = function (result) {
         console.log("Share completed? " + result.completed); // On Android apps mostly return false even while it's true
         console.log("Shared to app: " + result.app); // On Android result.app is currently empty. On iOS it's empty when sharing is cancelled (result.completed=false)
     }
-
     var onError = function (msg) {
         console.log("Sharing failed with message: " + msg);
+        if (window.navigator) {
+            navigator.notification.alert('No email client configured for sharing.');
+        }
     }
 
-    $scope.shareViaEmail1 = function () {
-
-        // check for a configured email client
-        window.plugins.socialsharing.canShareViaEmail( // share something
-            window.plugins.socialsharing.shareViaEmail(
-                '<html><p>Can we put HTML here?</p></html>',
-                'Share PDFs via email', ['jrgreen@gmail.com'], // TO: must be null or an array
-                null, // CC: must be null or an array
-                null, // BCC: must be null or an array
-                filesToShare, // FILES: null, a string, or an array
-                onSuccess, // called when email was sent or canceled, no way to differentiate
-                onError // called when something unexpected happened
-            ));
-
-
-
-    };
-
-    $scope.shareViaEmail2 = function () {
-
-        // check for a configured email client
-        window.plugins.socialsharing.canShareViaEmail( // share something
-            window.plugins.socialsharing.shareViaEmail(
-                'test links - http://google.com',
-                'Share PDFs via email', ['jrgreen@gmail.com'], // TO: must be null or an array
-                null, // CC: must be null or an array
-                null, // BCC: must be null or an array
-                null, // FILES: null, a string, or an array
-                onSuccess, // called when email was sent or canceled, no way to differentiate
-                onError // called when something unexpected happened
-            ));
-
-
-
-    };
-
-    $scope.shareViaEmail3 = function () {
-
-        // check for a configured email client
-        window.plugins.socialsharing.canShareViaEmail( // share something
-            window.plugins.socialsharing.shareViaEmail(
-                'test links - http://google.com<br> test2 - http://gmail.com',
-                'Share PDFs via email', ['jrgreen@gmail.com'], // TO: must be null or an array
-                null, // CC: must be null or an array
-                null, // BCC: must be null or an array
-                null, // FILES: null, a string, or an array
-                onSuccess, // called when email was sent or canceled, no way to differentiate
-                onError // called when something unexpected happened
-            ));
-
-
-
+    var createHTMLlinks = function (filesToShare) {
+        // create an HTML link in the following format:
+        // $FILENAME - <a href ="$URL">$URL</a>  
+        var html = '';
+        filesToShare.forEach(file => {
+            html += '<br>';
+            html += file.filename;
+            html += ' - <a href ="' + file.url + '">' + file.url + '</a>';
+        });
+        return html;
     };
 
 
